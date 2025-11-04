0fab3da7cef3925fd0f5a44db4b6ce1c66f55a23
marvidik <ebubeidika@gmail.com>
2025-11-04 19:11:26 +0100

    added the env path shower

--- mrzion/settings.py
+++ mrzion/settings.py
@@ -25,6 +25,8 @@ SITE_URL = os.getenv("SITE_URL")
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv(os.path.join(BASE_DIR, ".env"))
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
